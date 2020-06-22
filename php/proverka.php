f4d1ae854ba094213667ddf4172f19eb35779560
EternityJR <65914566+EternityJR@users.noreply.github.com>
2020-06-22 18:16:27 +0400

    Update proverka.php

--- php/proverka.php
+++ php/proverka.php
@@ -6,30 +6,15 @@
     $mail = filter_var(trim($_POST['mail']), FILTER_SANITIZE_STRING);
     $password = filter_var(trim($_POST['password']), FILTER_SANITIZE_STRING);
 
-    if(mb_strlen($name) < 2 || mb_strlen($name) > 20) {
-        echo "Недопустимая длина имени<br>";
-        echo "<a href='../index.php'>Вернуться назад</a>";
-        exit();
-    }
-    else if(mb_strlen($surname) < 3 || mb_strlen($surname) > 20) {
-        echo "Недопустимая длина фамилии (от 4 до 20)<br>";
-        echo "<a href='../index.php'>Вернуться назад</a>";
-        exit();
-    }
     
-    else if(mb_strlen($login) < 5 || mb_strlen($login) > 15) {
-        echo "Недопустимая длина логина (от 6 до 15)<br>";
-        echo "<a href='../index.php'>Вернуться назад</a>";
-        exit();
-    }
-    else if(mb_strlen($mail) < 6 || mb_strlen($mail) > 60) {
-        echo "Недопустимая длина почты<br>";
-        echo "<a href='../index.php'>Вернуться назад</a>";
+    if(mb_strlen($login) < 5 || mb_strlen($login) > 15) {
+        setcookie('wrlog', 15, time() + 250, "/");
+        header('location: /');
         exit();
     }
     else if(mb_strlen($password) < 5 || mb_strlen($password) > 32) {
-        echo "Недопустимая длина пароля (от 6 до 32)<br>";
-        echo "<a href='../index.php'>Вернуться назад</a>";
+        setcookie('wrpas', 15, time() + 250, "/");
+        header('location: /');
         exit();
     }
 
