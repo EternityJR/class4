df757ad10fb6102370127ea0c5e09c2d2a421357
EternityJR <65914566+EternityJR@users.noreply.github.com>
2020-06-22 18:14:47 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
+<?php
+            if ($_COOKIE['wrlog'] == ''){
+                $wrlog = true;
+                setcookie('wrlog', 15, time() - 250, "/");
+            }
+            if ($_COOKIE['wrpas'] == ''){
+                $wrpas = true;
+                setcookie('wrpas', 15, time() - 250, "/");
+            }
+        ?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
     
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/script.js"></script>
     <link rel="stylesheet" href="css/style.css">
     
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,8 +30,18 @@
             <input type="text" class="form-control" name="name" placeholder="Введите имя"><br>
             <input type="text" class="form-control" name="surname" placeholder="Введите фамилию"><br>
             <input type="text" class="form-control" name="login" placeholder="Введите логин"><br>
-            <input type="text" class="form-control" name="mail" placeholder="Введите почту"><br>
-            <input type="text" class="form-control" name="password" placeholder="Введите пароль"><br>
+            <?php
+            if ($wrlog == false){
+                echo("<p>Недопустимая длина логина (от 6 до 15)</p>");
+            }
+            ?>
+            <input type="email" class="form-control" name="mail" placeholder="Введите почту"><br>
+            <input type="password" class="form-control" name="password" placeholder="Введите пароль"><br>
+            <?php
+            if ($wrpas == false){
+                echo("<p>Недопустимая длина пароля (от 6 до 32)</p>");
+            }
+            ?>
             <button type="submit" class="btn btn-outline-success">Зарегистрироваться</button>
             <h6> Уже являетесь зарегистрированным пользователем? <a href="log.php">Войдите на сайт</a>
         </form>
