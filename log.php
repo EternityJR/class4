1853777c8601055cefe8c8a1c61c74683a939b4c
EternityJR <65914566+EternityJR@users.noreply.github.com>
2020-06-22 18:14:17 +0400

    Update log.php

--- log.php
+++ log.php
@@ -1,9 +1,18 @@
+<?php
+            if ($_COOKIE['wrong'] != ''){
+                $wrong = true;
+                setcookie('wrong', 15, time() - 250, "/");
+
+            }
+        ?>
+        <script>document.getElementById("Form").reset();</script>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
     
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/script.js"></script>
     <link rel="stylesheet" href="css/style.css">
 
     
@@ -12,13 +21,18 @@
 </head>
 <body>
     <div class="container">
-        <form action="/php/auth.php" method="post">
+        <form id="Form" action="php/auth.php" method="post">
             <h1>Зайди под своим именем</h1>
             <input type="text" class="form-control" name="login" placeholder="Введите логин"><br>
-            <input type="text" class="form-control" name="mail" placeholder="Введите почту"><br>
-            <input type="text" class="form-control" name="password" placeholder="Введите пароль"><br>
+            <input type="password" class="form-control" name="password" placeholder="Введите пароль"><br>
             <button type="submit" class="btn btn-outline-success">Войти</button>
         </form>
+        <?php
+            if ($wrong == true){
+                echo("<h1>Неправильный логин или пароль</h1>");
+            }
+        ?>
     </div>
+    
 </body>
 </html>
